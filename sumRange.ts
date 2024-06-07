1b74dd83f069515b811324383566f1255a605ffa
s.ziani <s.ziani@groupeonepoint.com>
2024-06-07 11:34:16 +0200

    exercice 3 proposition

--- sumRange.ts
+++
@@ -1,9 +0,0 @@
-export function sumRange(min: number, max: number): number {
-  if (min > max) {
-    return -1;
-  }
-  if (min === max) {
-    return min;
-  }
-  return min + sumRange(min + 1, max);
-}
